e32669161153d25cef0347b3eb7ccb843de58769
Ajo <andrej.bilec@gmail.com>
2018-08-19 17:48:39 +0200

    disconnected message added, periodical display restart added

--- home-arduino.ino
+++ home-arduino.ino
@@ -14,6 +14,9 @@ DHT dht5(DHTPIN_5, DHTTYPE);
 
 LiquidCrystal_I2C lcd(0x3f, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
 
+const bool TESTING = false;  // change to enable / disable testing
+int test_counter = 0;
+
 void setup() {
   Serial.begin(9600);
   Serial.println("setup()");
@@ -31,49 +34,58 @@ lcd.backlight();
   //lcd.home(); // go home
 }
 
-void printDhtOutput(int pin, float temperature, float humidity) {
+void printDhtOutput(const int pin, const float temperature, const float humidity) {  
+  const int row = pin - 2;
 
   if (isnan(temperature) || isnan(humidity)) {
+    lcd.setCursor(0, row);
+    lcd.print(" Senzor je odpojeny ");
+    
     Serial.print("Failed to read from dht");
     Serial.print(pin);
     Serial.println(" sensor!");
+    
     return;
   }
 
+  // print temperature
   char temperatureOutput[6];
   dtostrf(temperature, 5, 1, temperatureOutput);
-  
-  lcd.setCursor(0, pin - 2);
+  lcd.setCursor(0, row);
   lcd.print(temperatureOutput);
   lcd.print((char)223);
   lcd.print("C");
 
+  // clear the whitespace
+  lcd.print("       ");
+
+  // print humidity
   char humidityOutput[6];
   dtostrf(humidity, 5, 1, humidityOutput);
-
-  lcd.setCursor(14, pin - 2);
+  lcd.setCursor(14, row);
   lcd.print(humidityOutput);
-  lcd.print("%"); 
+  lcd.print("%");
 
   // print to console
-  //Serial.print("dht");
-  //Serial.print(pin);
-  //Serial.print(":\t");
+  Serial.print("dht");
+  Serial.print(pin);
+  Serial.print(":\t");
+  
+  Serial.print("temperature: ");
+  Serial.print(temperature);
+  Serial.print(" *C\t");
 
-  //Serial.print("temperature: ");
-  //Serial.print(temperature);
-  //Serial.print(" *C\t");
-
-  //Serial.print("humidity: ");
-  //Serial.print(humidity);
-  //Serial.println(" %"); 
+  Serial.print("humidity: ");
+  Serial.print(humidity);
+  Serial.println(" %"); 
 }
 
+int refresh_interval = 2000;
 int lcd_reset_counter = 0;
 
 void loop() {
   // Wait a few seconds between measurements.
-  delay(2000);
+  delay(refresh_interval);
 
   float dht2_temperature = dht2.readTemperature();
   float dht2_humidity = dht2.readHumidity();
@@ -87,9 +99,62 @@ void loop() {
   float dht5_temperature = dht5.readTemperature();
   float dht5_humidity = dht5.readHumidity();
 
+  // restart display periodically
   if (lcd_reset_counter >= 7200) {
     lcd_reset_counter = 0;
     lcd.begin (20, 4);
+  }
+
+  // TESTING FOR COMMON SCENARIOS
+  if (TESTING == true) {
+    refresh_interval = 500;
+    
+    // temperature scenarios
+    if (test_counter == 1) {
+      dht3_temperature = 0;
+    }
+    if (test_counter == 2) {
+      dht3_temperature = 9; 
+    }
+    if (test_counter == 3) {
+      dht3_temperature = 99; 
+    }
+    if (test_counter == 4) {
+      dht3_temperature = 9; 
+    }
+    if (test_counter == 5) {
+      dht3_temperature = -9;
+    }
+    if (test_counter == 6) {
+      dht3_temperature = -99;
+    }
+    if (test_counter == 7) {
+      dht3_temperature = -9;
+    }
+    if (test_counter == 8) {
+      dht3_temperature = NAN;
+    }
+    
+    // humidity scenarios
+    if (test_counter == 9) {
+      dht3_humidity = 0;
+    }
+    if (test_counter == 10) {
+      dht3_humidity = 50;
+    }
+    if (test_counter == 11) {
+      dht3_humidity = 100;
+    }
+    if (test_counter == 12) {
+      dht3_humidity = 50;
+    }
+    if (test_counter == 13) {
+      dht3_humidity = 0;
+    }
+    if (test_counter == 14) {
+      dht3_humidity = NAN;
+    }
+    test_counter++;
   }
 
   printDhtOutput(DHTPIN_2, dht2_temperature, dht2_humidity);
